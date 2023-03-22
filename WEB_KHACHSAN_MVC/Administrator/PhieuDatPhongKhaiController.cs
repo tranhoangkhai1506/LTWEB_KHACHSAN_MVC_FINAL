98573ca2f0aabaa2638d9bb5cc5ec69a64b5e8d7
Khải Trần Hoàng <khailtst@gmail.com>
2023-03-22 21:40:52 +0700

    Booking30%

---
+++ WEB_KHACHSAN_MVC/Administrator/PhieuDatPhongKhaiController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WEB_KHACHSAN_MVC.Models;
+
+namespace WEB_KHACHSAN_MVC.Administrator
+{
+    public class PhieuDatPhongKhaiController : Controller
+    {
+        KhachSanDBContextDataContext context = new KhachSanDBContextDataContext();
+        // GET: PhieuDatPhongKhai
+        public ActionResult ViewPhieuDatPhong()
+        {
+            var all_PDP = context.PHIEUDATPHONGs.ToList();
+            return View(all_PDP);
+        }
+        public ActionResult CreatePhieuDatPhong()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreatePhieuDatPhong(FormCollection collection, PHIEUDATPHONG phieuDat)
+        {
+            var E_SoNguoi = collection["SONGUOI"];
+            var E_NgayNhanPhong = collection["NGAYNHANPHONG"];
+            var E_NgayTraDuKien = collection["NGAYTRADUKIEN"];
+            var E_TienCoc = collection["TIENCOC"];
+            var E_MaPhong = collection["MAPHONG"];
+            var E_MaKH = collection["MAKH"];
+            var E_MaNV = collection["MANHANVIEN"];
+            if (string.IsNullOrEmpty(E_SoNguoi))
+            {
+                ViewData["Error"] = "Don't empty!";
+            }
+            else
+            {
+                phieuDat.SONGUOI = int.Parse(E_SoNguoi);
+                phieuDat.NGAYNHANPHONG = DateTime.Parse(E_NgayNhanPhong);
+                phieuDat.NGAYTRADUKIEN = DateTime.Parse(E_NgayTraDuKien);
+                phieuDat.TIENCOC = int.Parse(E_TienCoc);
+                phieuDat.MAPHONG = int.Parse(E_MaPhong);
+                phieuDat.MAKH = int.Parse(E_MaKH);
+                phieuDat.MANHANVIEN = int.Parse(E_MaNV);
+
+
+                context.PHIEUDATPHONGs.InsertOnSubmit(phieuDat);
+                context.SubmitChanges();
+                return RedirectToAction("ViewPhieuDatPhong");
+            }
+            return this.CreatePhieuDatPhong();
+        }
+
+        public ActionResult EditPhieuDatPhong(int maPhieuDatPhong)
+        {
+            var phieuDatPhong = context.PHIEUDATPHONGs.FirstOrDefault(m => m.MAPHIEUDATPHONG == (int)maPhieuDatPhong);
+            return View(phieuDatPhong);
+        }
+        [HttpPost]
+        public ActionResult EditPhieuDatPhong(int maPhieuDatPhong, FormCollection collection)
+        {
+            PHIEUDATPHONG phieuDat = context.PHIEUDATPHONGs.FirstOrDefault(p => p.MAPHIEUDATPHONG == maPhieuDatPhong);
+            var E_SoNguoi = collection["SONGUOI"];
+            var E_NgayNhanPhong = collection["NGAYNHANPHONG"];
+            var E_NgayTraDuKien = collection["NGAYTRADUKIEN"];
+            var E_TienCoc = collection["TIENCOC"];
+            var E_MaPhong = collection["MAPHONG"];
+            var E_MaKH = collection["MAKH"];
+            var E_MaNV = collection["MANHANVIEN"];
+            if (string.IsNullOrEmpty(E_SoNguoi))
+            {
+                ViewData["Error"] = "Don't empty!";
+            }
+            else
+            {
+                phieuDat.SONGUOI = int.Parse(E_SoNguoi);
+                phieuDat.NGAYNHANPHONG = DateTime.Parse(E_NgayNhanPhong);
+                phieuDat.NGAYTRADUKIEN = DateTime.Parse(E_NgayTraDuKien);
+                phieuDat.TIENCOC = int.Parse(E_TienCoc);
+                phieuDat.MAPHONG = int.Parse(E_MaPhong);
+                phieuDat.MAKH = int.Parse(E_MaKH);
+                phieuDat.MANHANVIEN = int.Parse(E_MaNV);
+                context.SubmitChanges();
+                return RedirectToAction("ViewPhieuDatPhong");
+            }
+            return this.EditPhieuDatPhong(maPhieuDatPhong);
+        }
+
+        public ActionResult DeletePhieuDatPhong(int maPhieuDatPhong)
+        {
+            var phieuDatPhong = context.PHIEUDATPHONGs.FirstOrDefault(m => m.MAPHIEUDATPHONG == maPhieuDatPhong);
+            return View(phieuDatPhong);
+        }
+        [HttpPost]
+        public ActionResult DeletePhieuDatPhong(int maPhieuDatPhong, FormCollection collection)
+        {
+            var phieuDP_Delete = context.PHIEUDATPHONGs.Where(m => m.MAPHIEUDATPHONG == maPhieuDatPhong).FirstOrDefault();
+            context.PHIEUDATPHONGs.DeleteOnSubmit(phieuDP_Delete);
+            context.SubmitChanges();
+            return RedirectToAction("ViewPhieuDatPhong");
+        }
+    }
+}
--- WEB_KHACHSAN_MVC/Controllers/BookingQuangController.cs
+++ WEB_KHACHSAN_MVC/Controllers/BookingQuangController.cs
@@ -3,15 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WEB_KHACHSAN_MVC.Models;
 
 namespace WEB_KHACHSAN_MVC.Controllers
 {
     public class BookingQuangController : Controller
     {
         // GET: BookingQuang
+        KhachSanDBContextDataContext context = new KhachSanDBContextDataContext();
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Booking(DateTime ngayNhanPhong, DateTime ngayTraDuKien, int soNguoi)
+        {
+            var listPhongTrong_BinhThuong = context.PHONGs.Where(p => p.TINHTRANG.Contains("Bình thường") && p.SONGUOITOIDA >= soNguoi).ToList();
+            var listPhongDangDuocThue = context.PHONGs.Where(p => p.TINHTRANG.Contains("Đang được thuê") && p.SONGUOITOIDA >= soNguoi).ToList();
+            List<PHIEUDATPHONG> listPhieuDatPhong = context.PHIEUDATPHONGs.ToList();
+            List<PHONG> listPhongSapDuocTra = new List<PHONG>();
+            foreach (var phongDangDuocThue in listPhongDangDuocThue)
+            {
+                foreach (var phieuDatPhongDangDuocThue in listPhieuDatPhong)
+                {
+                    if (phongDangDuocThue.MAPHONG == Convert.ToInt32(phieuDatPhongDangDuocThue.MAPHONG) && Convert.ToDateTime(phieuDatPhongDangDuocThue.NGAYTRADUKIEN) <= ngayNhanPhong)
+                    {
+                        listPhongSapDuocTra.Add(phongDangDuocThue);
+                    }
+                }
+            }
+            if (listPhongTrong_BinhThuong.Count() != 0)
+            {
+                return View(listPhongTrong_BinhThuong);
+            }
+            else if (listPhongSapDuocTra.Count() != 0)
+            {
+                return View(listPhongSapDuocTra);
+            }
+            return View(listPhongSapDuocTra);
         }
     }
 }
