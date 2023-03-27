e0dc6753655f286916e76b6b0654f66fb4340014
Nguyễn Ngọc Huy <nguyenngohuy112@gmail.com>
2023-03-28 00:29:40 +0700

    contact_service_Huy

--- WEB_KHACHSAN_MVC/Controllers/ServiceHuyController.cs
+++ WEB_KHACHSAN_MVC/Controllers/ServiceHuyController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WEB_KHACHSAN_MVC.Models;
 
 namespace WEB_KHACHSAN_MVC.Controllers
 {
     public class ServiceHuyController : Controller
     {
+        KhachSanDBContextDataContext context = new KhachSanDBContextDataContext();
         // GET: ServiceHuy
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult ViewService()
+        {
+            var all_service = context.DICHVUs.ToList();
+            return View(all_service);
         }
     }
 }
